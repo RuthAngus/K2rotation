b3c42f933b680bf6bebb9653b50d8d6d9cf7185c
Dan F-M <danfm@nyu.edu>
2015-02-19 15:10:07 -0500

    swithcing to s/n

--- test.py
+++ test.py
@@ -1,25 +1,30 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import fitsio
 import h5py
 import time
+from scipy.signal import lombscargle
 
-data = fitsio.read("data/ktwo201374602-c01_lpd-lc.fits")
-aps = fitsio.read("data/ktwo201374602-c01_lpd-lc.fits", 2)
+vdb = pd.read_csv("data/ep201859398corcut.csv", index_col=False,
+                  names=["time", "flux"], skiprows=1)
+
+fn = "data/ktwo201859398-c01_lpd-lc.fits"
+data = fitsio.read(fn)
+aps = fitsio.read(fn, 2)
 y = data["flux"][:, np.argmin(aps["cdpp6"])]
 x = data["time"]
 q = data["quality"]
-l = np.isfinite(y) * np.isfinite(x) * (q==0)
+l = np.isfinite(y) * np.isfinite(x) * (q == 0)
 y, x = y[l], x[l]
 y /= np.median(y)
 
-y += .0005*np.sin(.1*2*np.pi*x)
-
 with h5py.File("data/c1.h5", "r") as f:
-    basis = f["basis"][:150, l]
+    basis = f["basis"][:20, l]
 
 AT = np.concatenate((basis, np.ones((3, len(y)))), axis=0)
 ATA = np.dot(AT, AT.T)
+
 
 def eval_freq(f, compute_trends=False):
     arg = 2*np.pi*f*x
@@ -37,11 +42,14 @@ def eval_freq(f, compute_trends=False):
     ATA[-2:, -2:] = np.dot(AT[-2:, :], AT[-2:, :].T)
 
     w = np.linalg.solve(ATA, np.dot(AT, y))
+    S = np.linalg.inv(ATA)[-2:, -2:]
+    s2n = np.dot(w[-2:], np.linalg.solve(S, w[-2:]))
 
     if compute_trends:
         trends = np.dot(w[:-2], AT[:-2])
         return np.sum(w[-2:]**2), trends
-    return np.sum(w[-2:]**2)
+    return s2n
+
 
 # Plotting the true model.
 a, t = eval_freq(0.1, compute_trends=True)
@@ -54,7 +62,8 @@ plt.subplot(2, 1, 2)
 plt.plot(x, y-t, "r")
 plt.savefig("data")
 
-fs = np.linspace(.0125, .2, 10000)
+fs = np.linspace(.05, .5, 10000)
+p = lombscargle(np.array(vdb.time), np.array(vdb.flux) - 1.0, 2*np.pi*fs)
 A = np.zeros_like(fs)
 strt = time.time()
 for i, f in enumerate(fs):
@@ -62,7 +71,8 @@ for i, f in enumerate(fs):
 print time.time() - strt
 
 plt.clf()
-plt.plot(fs, A)
+plt.plot(fs, A / A.max())
+plt.plot(fs, p / p.max())
 plt.yscale("log")
 plt.savefig("pgram")
 
