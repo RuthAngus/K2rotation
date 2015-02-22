7c6c399e56f987d94be453fe71b80c4e8eba2fe5
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-02-22 12:31:03 -0500

    sine injections

--- generate_lcs.py
+++ generate_lcs.py
@@ -5,30 +5,46 @@ import pyfits
 from mklc import mklc
 import fitsio
 
-fname = 201300080
+def inject_star_spots(raw_x, periods):
+    amps = []
+    for i, p in enumerate(periods):
+        # generate injection
+        nspot = 200
+        incl = np.pi*5./12.
+        amp = 1.
+        tau = 30.5
+        res0, res1 = mklc(raw_x, nspot, incl, amp, tau, p)
+        time = res1[0]
+        flux = res1[2]/np.median(res1[2]) - 1
+        np.savetxt("injections/%s_lc.txt" % i, np.transpose((time, flux)))
+        amps.append(res0[-1])
+    np.savetxt("injections/truth.txt", np.transpose((range(len(periods)),
+               periods, np.array(amps))))
 
-# load K2 light curve
-data = fitsio.read("data/ktwo%s-c01_lpd-lc.fits" % fname)
-aps = fitsio.read("data/ktwo%s-c01_lpd-lc.fits" % fname, 2)
-raw_y = data["flux"][:, np.argmin(aps["cdpp6"])]
-raw_x = data["time"]
-q = data["quality"]
-l = np.isfinite(raw_y) * np.isfinite(raw_x) * (q==0)
-raw_y, raw_x = raw_y[l], raw_x[l]
-raw_y /= np.median(raw_y)
-raw_y -= 1
+def inject_sine_wave(raw_x, periods):
+    amps = []
+    for i, p in enumerate(periods):
+        flux = np.sin(raw_x * (1./p)*2*np.pi)
+        np.savetxt("injections/sine/%s_lc.txt" % i,
+                   np.transpose((raw_x, flux)))
+    np.savetxt("injections/sine/truth.txt", np.transpose((range(len(periods)),
+               periods)))
 
-periods = np.exp(np.random.uniform(np.log(1), np.log(70), 100))
-for i, p in enumerate(periods):
+if __name__ == "__main__":
 
-    # generate injection
-    nspot = 200
-    incl = np.pi*5./12.
-    amp = 1.
-    tau = 30.5
-    res0, res1 = mklc(raw_x, nspot, incl, amp, tau, p)
-    time = res1[0]
-    flux = res1[2]/np.median(res1[2]) - 1
-    np.savetxt("injections/%s_lc.txt" % i, np.transpose((time, flux)))
+    fname = 201300080
 
-np.savetxt("truth.txt", np.transpose((range(len(periods)), periods)))
+    # load K2 light curve
+    data = fitsio.read("data/ktwo%s-c01_lpd-lc.fits" % fname)
+    aps = fitsio.read("data/ktwo%s-c01_lpd-lc.fits" % fname, 2)
+    raw_y = data["flux"][:, np.argmin(aps["cdpp6"])]
+    raw_x = data["time"]
+    q = data["quality"]
+    l = np.isfinite(raw_y) * np.isfinite(raw_x) * (q==0)
+    raw_y, raw_x = raw_y[l], raw_x[l]
+    raw_y /= np.median(raw_y)
+    raw_y -= 1
+
+    periods = np.arange(1., 70., .5)
+#     inject_star_spots(raw_x, periods)
+    inject_sine_wave(raw_x, periods)
--- injections.py
+++ injections.py
@@ -10,6 +10,7 @@ from K2pgram import K2pgram
 from colours import plot_colours
 cols = plot_colours()
 from gatspy.periodic import LombScargle
+import glob
 
 plotpar = {'axes.labelsize': 10,
            'text.fontsize': 10,
@@ -19,68 +20,91 @@ plotpar = {'axes.labelsize': 10,
            'text.usetex': True}
 plt.rcParams.update(plotpar)
 
-fname = 201300080
+def grid_over_amps(basis, flux, raw_x, raw_y, truth, fs, amps, true_a,
+                   plot=False):
 
-# load K2 light curve
-data = fitsio.read("data/ktwo%s-c01_lpd-lc.fits" % fname)
-aps = fitsio.read("data/ktwo%s-c01_lpd-lc.fits" % fname, 2)
-raw_y = data["flux"][:, np.argmin(aps["cdpp6"])]
-raw_x = data["time"]
-q = data["quality"]
-l = np.isfinite(raw_y) * np.isfinite(raw_x) * (q==0)
-raw_y, raw_x = raw_y[l], raw_x[l]
-raw_y /= np.median(raw_y)
-raw_y -= 1
+    K2P, rawP, K2a, rawa = [], [], [], []
+    print len(amps)
+    for i, a in enumerate(amps):
+        print i
 
-# generate injection
-nspot = 200
-incl = np.pi*5./12.
-amp = 1.
-tau = 30.5
-p = 10.0,
-res0, res1 = mklc(raw_x)
-time = res1[0]
-flux = res1[2]/np.median(res1[2]) - 1
+        # add lcs together
+        fx = flux * a
+        y = fx + raw_y
+        SN = np.var(fx) / np.var(raw_y)
 
-# add lcs together
-a = .01
-flux *= a
-y = flux + raw_y
+        # construct arrays
+        AT = np.concatenate((basis, np.ones((3, len(y)))), axis=0)
+        ATA = np.dot(AT, AT.T)
 
-SN = np.var(flux) / np.var(raw_y)
-print "signal = ", np.var(flux)
-print "noise = ", np.var(raw_y)
-print "S/N = ", SN
+        # calculate K2pgram
+        _K2pgram = K2pgram(raw_x, y, fs, AT, ATA)
+        l = _K2pgram==max(_K2pgram)
+        if truth-.1*truth < fs[l][0] and fs[l][0] < truth+.1*truth:
+            K2P.append(fs[l][0])
+            K2a.append(a*true_a)
 
-# load basis
-with h5py.File("data/c1.h5", "r") as f:
-    basis = f["basis"][:10, l]
+        # calculate periodogram of raw light curve
+        y = np.array([_y.astype("float64") for _y in y])
+        raw_x = np.array([_raw_x.astype("float64") for _raw_x in raw_x])
+        pgram = sps.lombscargle(raw_x, y, 2*np.pi*fs)
+        l = pgram==max(pgram)
+        if truth-.1*truth < fs[l][0] and fs[l][0] < truth+.1*truth:
+            rawP.append(fs[l][0])
+            rawa.append(a*true_a)
 
-# construct arrays
-AT = np.concatenate((basis, np.ones((3, len(y)))), axis=0)
-ATA = np.dot(AT, AT.T)
+        if plot == True:
+            plt.clf()
+            plt.subplot(3, 1, 1)
+            plt.plot(raw_x, raw_y, "k.")
+            plt.plot(raw_x, fx, color=cols.green)
+            plt.subplot(3, 1, 2)
+            plt.plot(raw_x, y, "k.")
+            plt.subplot(3, 1, 3)
+            plt.axvline(truth, color=".7", linestyle="--")
+            plt.plot(fs, _K2pgram/max(_K2pgram), color=cols.blue,
+                     label="$\mathrm{K2pgram$}")
+            plt.plot(fs, pgram/max(pgram), color=cols.pink,
+                     label="$\mathrm{raw pgram}$")
+            plt.savefig("result")
+            raw_input('enter')
+    return np.array(K2a), np.array(K2P), np.array(rawa), np.array(rawP)
 
-# calculate K2pgram
-print "calculating K2pgram..."
-fs = np.arange(1./20, 1./2, .001)
-_K2pgram = K2pgram(raw_x, y, fs, AT, ATA)
+if __name__ == "__main__":
 
-# calculate periodogram of raw light curve
-print "calculating raw pgram..."
-y = np.array([i.astype("float64") for i in y])
-raw_x = np.array([i.astype("float64") for i in raw_x])
-pgram = sps.lombscargle(raw_x, y, 2*np.pi*fs)
+    fname = 201300080
 
-# plot
-plt.clf()
-plt.subplot(3, 1, 1)
-plt.plot(raw_x, raw_y, "k.")
-plt.plot(raw_x, flux, color=cols.green)
-plt.subplot(3, 1, 2)
-plt.plot(raw_x, y, "k.")
-plt.subplot(3, 1, 3)
-plt.axvline(1./10, color=".7", linestyle="--")
-plt.plot(fs, _K2pgram/max(_K2pgram), color=cols.blue,
-         label="$\mathrm{K2pgram$}")
-plt.plot(fs, pgram/max(pgram), color=cols.pink, label="$\mathrm{raw pgram}$")
-plt.savefig("%s_result" % fname)
+    # load K2 light curve
+    data = fitsio.read("data/ktwo%s-c01_lpd-lc.fits" % fname)
+    aps = fitsio.read("data/ktwo%s-c01_lpd-lc.fits" % fname, 2)
+    raw_y = data["flux"][:, np.argmin(aps["cdpp6"])]
+    raw_x = data["time"]
+    q = data["quality"]
+    l = np.isfinite(raw_y) * np.isfinite(raw_x) * (q==0)
+    raw_y, raw_x = raw_y[l], raw_x[l]
+    raw_y /= np.median(raw_y)
+    raw_y -= 1
+
+    # load basis
+    with h5py.File("data/c1.h5", "r") as f:
+        basis = f["basis"][:150, l]
+
+    # load truths
+    name, true_p, true_a = np.genfromtxt("truth.txt").T
+
+    # load injections
+    fnames = glob.glob("injections/*_lc.txt")
+
+    fs = np.arange(.08, .3, .001)
+    for i, fname in enumerate(fnames):
+        amps = np.arange(.0001, .01, .0001)
+        print true_p[i], true_a[i]
+        time, flux = np.genfromtxt(fname).T
+        K2a, K2P, rawa, rawP = grid_over_amps(basis, flux, raw_x, raw_y,
+                                              true_p[i], fs, amps, true_a[i],
+                                              plot=True)
+        print K2a
+        abins = np.linspace(min(amps), max(amps), 10)
+#         print np.digitize(K2a, abins)
+        print np.histogram(K2a)
+        assert 0
