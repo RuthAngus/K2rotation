ec0295a85a8ffc4fc3656f7bc49fac55e6413546
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-03-04 13:02:27 -0500

    initial

---
+++ plots/K2pgram_rotation_poster_child.py
@@ -0,0 +1,174 @@
+# This script creates the K2_rotation_poster_child.pdf figure
+# it can also plot the FFT of the eigen light curves,
+# the 2nd K2pgram (periodogram of a 2nd frequency),
+# the data conditioned on the best freqency and the top
+# eigen light curves.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import fitsio
+from K2pgram import K2pgram, eval_freq
+from rotation_poster_child import max_peak_detect
+import h5py
+from gatspy.periodic import LombScargle
+
+plotpar = {'axes.labelsize': 12,
+           'text.fontsize': 10,
+           'legend.fontsize': 10,
+           'xtick.labelsize': 8,
+           'ytick.labelsize': 8,
+           'text.usetex': True}
+plt.rcParams.update(plotpar)
+
+def bases_FFT(x, y, fs, basis):
+    ps = 1./fs
+    plt.clf()
+    cols = np.linspace(.1, .99, len(basis))
+    for i in range(len(basis)):
+        model = LombScargle().fit(x, basis[i], np.ones_like(y)*1e-5)
+        pgram = model.periodogram(ps)
+        plt.plot(fs, pgram, color="%s" % cols[i])
+    plt.savefig("all_bases")
+
+def K2pgram2(x, y, fs, basis):
+    # construct arrays
+    AT = np.concatenate((basis, np.ones((5, len(y)))), axis=0)
+    ATA = np.dot(AT, AT.T)
+
+    # compute 2nd k2pgram
+    s2n = K2pgram2(x, y, mx, fs, AT, ATA)
+
+    plt.clf()
+    plt.subplot(2, 1, 1)
+    plt.plot(x, y, "k")
+    plt.xlim(min(x), max(x))
+    plt.xlabel("$\mathrm{BJD-2454833~(days)}$")
+    plt.ylabel("$\mathrm{Normalized~Flux}$")
+
+    plt.subplot(2, 1, 2)
+    plt.plot(fs, s2n, "k")
+    plt.xlabel("$\mathrm{Frequency~(days}^{-1}\mathrm{)}$")
+#     plt.ylabel("$\mathrm{S/N}$")
+    plt.ylabel("$\mathrm{Power}$")
+    plt.subplots_adjust(hspace=.4)
+    mx, my = max_peak_detect(fs, s2n)
+    plt.axvline(mx, color=".5", linestyle="--", label="$P_{rot}=%.2f$" % mx)
+    plt.savefig("../documents/K22_rotation_poster_child.pdf")
+
+# plot the best frequencies
+def plot_best(x, y, fs, AT, ATA, mx):
+    plt.clf()
+    f = mx
+    plt.subplot(2, 1, 1)
+    s2n, trends = eval_freq(x, y, f, AT, ATA, compute_trends=True)
+    plt.plot(x, y-trends, "k")
+    plt.subplot(2, 1, 2)
+    s2n, trends = eval_freq(x, y, f, AT, ATA, compute_trends=True)
+    plt.plot(x, y, x, trends)
+    plt.savefig("test1")
+
+    plt.clf()
+    f = mx
+    plt.subplot(2, 1, 1)
+    s2n, trends = eval_freq(x, y, 1./10.52, AT, ATA, compute_trends=True)
+    plt.plot(x, y-trends, "k")
+    plt.subplot(2, 1, 2)
+    s2n, trends = eval_freq(x, y, 1./10.52, AT, ATA, compute_trends=True)
+    plt.plot(x, y, x, trends)
+    plt.savefig("test2")
+
+def K2_poster_child_plot(x, y, fs, s2n, epid):
+
+    x2, y2, _ = np.genfromtxt("/Users/angusr/data/K2/c1lcsr4/ep%s.csv" % epid,
+                              delimiter=",").T
+    plt.clf()
+    plt.subplot(2, 1, 1)
+    plt.plot(x, y, "k")
+#     plt.plot(x2, y2-1)
+    plt.xlim(min(x), max(x))
+    plt.xlabel("$\mathrm{BJD-2454833~(days)}$")
+    plt.ylabel("$\mathrm{Normalized~Flux}$")
+
+    plt.subplot(2, 1, 2)
+    plt.plot(fs, s2n, "k")
+    plt.xlabel("$\mathrm{Frequency~(days}^{-1}\mathrm{)}$")
+    plt.ylabel("$\mathrm{Power}$")
+#     plt.ylabel("$\mathrm{S/N}$")
+    plt.ylim(0, 4e-5)
+    plt.subplots_adjust(hspace=.4)
+    plt.axvline(mx, color=".5", linestyle="--",
+                label="$P_{rot}=%.2f ~\mathrm{days}$" % (1./mx))
+    plt.legend()
+    plt.savefig("../documents/K2_rotation_%s.pdf" % epid)
+
+# plot the top 5 components
+def top_5(x, basis, w):
+    sw = np.sort(w)
+    print sw
+    print sw[0]
+    l = np.arange(len(w))[w == sw[0]][0]
+    print l
+    plt.clf()
+    plt.subplot(5, 1, 1)
+    plt.plot(x, basis[l, :])
+    plt.subplot(5, 1, 2)
+    l = np.arange(len(w))[w == sw[1]][0]
+    print l
+    plt.plot(x, basis[l, :])
+    plt.subplot(5, 1, 3)
+    l = np.arange(len(w))[w == sw[2]][0]
+    print l
+    plt.plot(x, basis[l, :])
+    plt.subplot(5, 1, 4)
+    l = np.arange(len(w))[w == sw[3]][0]
+    print l
+    plt.plot(x, basis[l, :])
+    plt.subplot(5, 1, 5)
+    l = np.arange(len(w))[w == sw[4]][0]
+    print l
+    plt.plot(x, basis[l, :])
+    plt.show()
+
+if __name__ == "__main__":
+
+    epid = "201317002"
+    # epid = "201372313"
+    # epid = "201310077"
+    # epid = "201315808"
+    # epid = "201370145"
+    # epid = "201318479"
+    # epid = "201310768"
+
+    data = fitsio.read("../data/ktwo%s-c01_lpd-lc.fits" % epid)
+    aps = fitsio.read("../data/ktwo%s-c01_lpd-lc.fits" % epid, 2)
+    y = data["flux"][:, np.argmin(aps["cdpp6"])]
+    x = data["time"]
+    q = data["quality"]
+    l = np.isfinite(y) * np.isfinite(x) * (q==0)
+    y, x = y[l], x[l]
+    y /= np.median(y)
+    y -= 1
+
+    # load basis
+    with h5py.File("../data/c1.h5", "r") as f:
+        basis = f["basis"][:150, l]
+
+    # construct arrays
+    AT = np.concatenate((basis, np.ones((3, len(y)))), axis=0)
+    ATA = np.dot(AT, AT.T)
+
+    # # compute K2 pgram
+    # try:
+    #     fs, s2n = np.genfromtxt("%spgram.txt" % epid).T
+    #     print "periodogram file found"
+    # except:
+    #     fs = np.linspace(1e-6, .7, 1000)
+    # #    fs = np.linspace(.1, 1.7, 1000)
+    #     np.savetxt("%spgram.txt" % epid, np.transpose((fs, s2n)))
+    # fs = np.linspace(.01, .7, 1000)
+    fs = np.linspace(1e-6, .7, 1000)
+    amp2s, s2n  = K2pgram(x, y, fs, AT, ATA)
+    mx, my = max_peak_detect(fs, amp2s)
+
+    # K2_poster_child_plot(x, y, fs, s2n, epid)
+    K2_poster_child_plot(x, y, fs, amp2s, epid)
---
+++ plots/rotation_poster_child.py
@@ -0,0 +1,65 @@
+# This script creates the rotation_poster_child.pdf figure
+
+import numpy as np
+import matplotlib.pyplot as plt
+import emcee
+from gatspy.periodic import LombScargle
+
+plotpar = {'axes.labelsize': 12,
+           'text.fontsize': 10,
+           'legend.fontsize': 10,
+           'xtick.labelsize': 8,
+           'ytick.labelsize': 8,
+           'text.usetex': True}
+plt.rcParams.update(plotpar)
+
+def max_peak_detect(x, y):
+    peaks = np.array([i for i in range(1, len(x)-1) if y[i-1] < y[i] and
+                     y[i+1] < y[i]])
+    l = y[peaks] == max(y[peaks])
+    return x[peaks][l][0], y[peaks][l][0]
+
+# load poster child light curve
+x, y, _ = np.genfromtxt("../data/ep201317002.csv", delimiter=",").T
+y = y/np.median(y) - 1
+
+# compute acf
+dt = 0.02043359821692  # time resolution of K2 data (from header)
+acf = emcee.autocorr.function(y)
+lags = np.arange(len(acf)) * dt
+
+# compute LS periodogram
+model = LombScargle().fit(x, y, np.ones_like(y)*1e-5)
+fmax = max(lags)/100.
+fs = np.linspace(1e-6, fmax, 1000)
+ps = 1./fs
+pgram = model.periodogram(ps)
+
+plt.clf()
+plt.subplot(3, 1, 1)
+plt.plot(x, y, "k")
+plt.xlabel("$\mathrm{BJD-2454833~(days)}$")
+plt.ylabel("$\mathrm{Normalized~Flux}$")
+plt.xlim(min(x), max(x))
+plt.ylim(-.02, .02)
+
+plt.subplot(3, 1, 2)
+plt.plot(lags, acf, "k")
+plt.ylabel("$\mathrm{Autocorrelation}$")
+plt.xlabel("$\mathrm{Time~(days)}$")
+mx, my = max_peak_detect(lags, acf)
+plt.axvline(mx, color=".5", linestyle="--", label="$P_{rot}=%.2f$" % mx)
+plt.legend()
+plt.xlim(min(lags), max(lags))
+
+plt.subplot(3, 1, 3)
+plt.plot(fs, pgram, "k")
+plt.xlabel("$\mathrm{Frequency~(days}^{-1}\mathrm{)}$")
+plt.ylabel("$\mathrm{Power}$")
+mx, my = max_peak_detect(fs, pgram)
+plt.xlim(min(fs), fmax)
+px = 1./mx
+plt.axvline(mx, color=".5", linestyle="--", label="$P_{rot}=%.2f$" % px)
+plt.legend()
+plt.subplots_adjust(hspace=.4)
+plt.savefig("../documents/rotation_poster_child.pdf")
