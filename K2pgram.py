b443bb54f5362cf87034b5f1d7ca4bc869d31960
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-03-04 13:12:36 -0500

    adding 2nd pgram

--- K2pgram.py
+++ K2pgram.py
@@ -2,14 +2,27 @@ import numpy as np
 
 # loop over eval_freq to compute a periodogram
 def K2pgram(x, y, fs, AT, ATA):
-    pgram = np.zeros_like(fs)
+    """
+    # `K2pgram`
+
+    Calls eval_freq on x and y for each frequency in fs
+    Returns the sum of squared amplitudes and the signal to noise
+    """
     amps2 = np.zeros_like(fs)
+    s2n = np.zeros_like(fs)
     for i, f in enumerate(fs):
-        pgram[i], amps2[i] = eval_freq(x, y, f, AT, ATA)
-    return pgram, amps2
+        amps2[i], s2n[i] = eval_freq(x, y, f, AT, ATA)
+    return amps2, s2n
 
 # calculate periodogram by just updating sin and cos parts of the  matrices
 def eval_freq(x, y, f, AT, ATA, compute_trends=False):
+    """
+    # `eval_freq`
+
+    Computes the sum of squared amplitudes for a sine and cosine function
+    with frequency, f.
+    Linear least squares fit to the data.
+    """
     arg = 2*np.pi*f*x
     AT[-2, :] = np.sin(arg)
     AT[-1, :] = np.cos(arg)
@@ -35,32 +48,53 @@ def eval_freq(x, y, f, AT, ATA, compute_trends=False):
 
 # loop over eval_freq to compute a periodogram
 def K2pgram_basis(x, y, fs, AT, ATA):
-    pgram = np.zeros_like(fs)
+    """
+    # `K2pgram`
+
+    Calls eval_freq_basis on x and y for each frequency in fs
+    Returns the sum of squared amplitudes and the signal to noise
+    """
+    amp2s = np.zeros_like(fs)
     for i, f in enumerate(fs):
-        pgram[i], s2n = eval_freq_basis(x, y, f, AT, ATA)
-    return pgram, s2n
+        amp2s[i] = eval_freq_basis(x, y, f, AT, ATA)
+    return amp2s
 
 def eval_freq_basis(x, y, f, AT, ATA, compute_trends=False):
+    """
+    # `eval_freq_basis`
+
+    Same as eval_freq but doesn't make use of the sparcity
+    """
     arg = 2*np.pi*f*x
     AT[-2, :] = np.sin(arg)
     AT[-1, :] = np.cos(arg)
-
     ATA = np.dot(AT, AT.T)
     w = np.linalg.solve(ATA, np.dot(AT, y))
-
-    return np.sum(w[-2:]**2), w
+    return np.sum(w[-2:]**2)
 
 # loop over eval_freq to compute a periodogram.
 # f is the frequency you already found
 def K2pgram2(x, y, f1, fs, AT, ATA):
-    pgram = np.zeros_like(fs)
+    """
+    # `K2pgram2`
+
+    Calls eval_2nd_freq on x and y for each frequency in fs
+    Returns the sum of squared amplitudes and the signal to noise
+    """
+    amp2s = np.zeros_like(fs)
     for i, f2 in enumerate(fs):
         if f1 != f2:
-            pgram[i] = eval_2nd_freq(x, y, f1, f2, AT, ATA)
-    return pgram
+            amp2s[i] = eval_2nd_freq(x, y, f1, f2, AT, ATA)
+    return amp2s
 
 # calculate periodogram by just updating sin and cos parts of the  matrices
 def eval_2nd_freq(x, y, f1, f2, AT, ATA, compute_trends=False):
+    """
+    # `eval_2nd_freq`
+
+    Same as eval_freq but for 2 frequencies at once.
+    Just returns signal to noise
+    """
     arg1 = 2*np.pi*f1*x
     arg2 = 2*np.pi*f2*x
     AT[-4, :] = np.sin(arg1)
