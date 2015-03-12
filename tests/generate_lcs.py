319fa353d9e66557846538c75584e2f166355612
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-03-12 10:03:25 -0400

    examining power spectra

--- tests/generate_lcs.py
+++ tests/generate_lcs.py
@@ -4,6 +4,8 @@ import glob
 import pyfits
 from mklc import mklc
 import fitsio
+import h5py
+from K2pgram import K2pgram
 
 def inject_star_spots(raw_x, periods):
     amps = []
@@ -34,21 +36,35 @@ def inject_sine_wave(raw_x, periods, flag):
 
 if __name__ == "__main__":
 
-    fname = 201295312
+    fnames = [201310650, 201311700, 201311941]
 
-    # load K2 light curve
-    data = fitsio.read("../data/c1/ktwo%s-c01_lpd-lc.fits" % fname)
-    aps = fitsio.read("../data/c1/ktwo%s-c01_lpd-lc.fits" % fname, 2)
-    raw_y = data["flux"][:, np.argmin(aps["cdpp6"])]
-    raw_x = data["time"]
-    q = data["quality"]
-    l = np.isfinite(raw_y) * np.isfinite(raw_x) * (q==0)
-    raw_y, raw_x = raw_y[l], raw_x[l]
-    raw_y /= np.median(raw_y)
-    raw_y -= 1
+    for fname in fnames:
+        print fname
+        # load K2 light curve
+        data = fitsio.read("../data/c1/ktwo%s-c01_lpd-lc.fits" % fname)
+        aps = fitsio.read("../data/c1/ktwo%s-c01_lpd-lc.fits" % fname, 2)
+        raw_y = data["flux"][:, np.argmin(aps["cdpp6"])]
+        raw_x = data["time"]
+        q = data["quality"]
+        l = np.isfinite(raw_y) * np.isfinite(raw_x) * (q==0)
+        raw_y, raw_x = raw_y[l], raw_x[l]
+        raw_y /= np.median(raw_y)
+        raw_y -= 1
 
-#     periods = np.arange(1., 70., .5)  # rotation periods
-    f1 = 10. * 1e-6 * 24 * 3600
-    f2 = 300. * 1e-6 * 24 * 3600
-    periods = np.linspace(1./f1, 1./f2, 150)  # asteroseismology
-    inject_sine_wave(raw_x, periods, "a")
+        # load basis
+        with h5py.File("../data/c1.h5", "r") as f:
+            basis = f["basis"][:150, l]
+
+        # look at the K2pgram
+        periods = np.linspace(1., 70., 1000)  # rotation and astero
+        fs = 1./periods
+        amp2s, s2n, w = K2pgram(raw_x, raw_y, basis, fs)
+        plt.clf()
+        plt.plot(1./fs, s2n)
+        plt.show()
+
+    periods = np.arange(1., 70., .5)  # rotation periods
+#     f1 = 10. * 1e-6 * 24 * 3600
+#     f2 = 300. * 1e-6 * 24 * 3600
+#     periods = np.linspace(1./f1, 1./f2, 150)  # asteroseismology
+    inject_sine_wave(raw_x, periods, "r")
