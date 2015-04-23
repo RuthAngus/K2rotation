33bc6f21846abb1cfda0401285fe6fddf895298c
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-04-23 11:32:33 -0400

    adding SIP

--- plots/raw_and_vbg.py
+++ plots/raw_and_vbg.py
@@ -3,63 +3,87 @@ import matplotlib.pyplot as plt
 from gatspy.periodic import LombScargle
 import fitsio
 
-plotpar = {'axes.labelsize': 18,
-           'text.fontsize': 18,
-           'legend.fontsize': 18,
-           'xtick.labelsize': 18,
-           'ytick.labelsize': 18,
-           'text.usetex': True}
-plt.rcParams.update(plotpar)
+def raw_and_vbg():
 
-# eid = "201545182"
-eid = "201183188"
-fname = "../data/c1/ktwo%s-c01_lpd-lc.fits" % eid
+    plotpar = {'axes.labelsize': 14,
+               'text.fontsize': 18,
+               'legend.fontsize': 18,
+               'xtick.labelsize': 14,
+               'ytick.labelsize': 16,
+               'text.usetex': True}
+    plt.rcParams.update(plotpar)
 
-# load raw data
-data = fitsio.read(fname)
-aps = fitsio.read(fname, 2)
-y = data["flux"][:, np.argmin(aps["cdpp6"])]
-x = data["time"]
-q = data["quality"]
-l = np.isfinite(y) * np.isfinite(x) * (q==0)
-y, x = y[l], x[l]
-y /= np.median(y)
-y -= 1
-x *= 24*3600  # convert to seconds
+    # eid = "201545182"
+    eid = "201183188"
+    fname = "../data/c1/ktwo%s-c01_lpd-lc.fits" % eid
 
-fs = np.arange(.1, 300, 4e-2) * 1e-6  # astero
+    # load raw data
+    data = fitsio.read(fname)
+    aps = fitsio.read(fname, 2)
+    y = data["flux"][:, np.argmin(aps["cdpp6"])]
+    x = data["time"]
+    q = data["quality"]
+    l = np.isfinite(y) * np.isfinite(x) * (q==0)
+    y, x = y[l], x[l]
+    y /= np.median(y)
+    y -= 1
+    x *= 24*3600  # convert to seconds
 
-# plot raw data
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax1 = fig.add_subplot(211)
-model = LombScargle().fit(x, y, np.ones_like(y)*1e-5)
-period = 1. / fs
-raw_pgram = model.periodogram(period)
-ax1.plot(fs[::3]*1e6, raw_pgram[::3], "k")
-ax.set_title("$\mathrm{EPIC~%s}$" % eid)
-ax1.set_xlim(10, 280)
-ax1.set_ylim(0, .015)
+    fs = np.arange(.1, 300, 4e-2) * 1e-6  # astero
 
-ax.spines['top'].set_color('none')
-ax.spines['bottom'].set_color('none')
-ax.spines['left'].set_color('none')
-ax.spines['right'].set_color('none')
-ax.tick_params(labelcolor='w', top='off', bottom='off', left='off',
-               right='off')
+    # plot raw data
+    fig = plt.figure()
+    ax = fig.add_subplot(211)
+    ax1 = fig.add_subplot(311)
+    model = LombScargle().fit(x, y, np.ones_like(y)*1e-5)
+    period = 1. / fs
+    raw_pgram = model.periodogram(period)
+    ax1.plot(fs[::3]*1e6, raw_pgram[::3], "k")
+    ax.set_title("$\mathrm{EPIC~%s}$" % eid)
+    ax1.set_xlim(10, 280)
+    ax1.set_ylim(0, .015)
+    plt.ylabel("$\mathrm{Power}$")
+    ticks = ax1.get_yticks()
+    print ticks
+    ax1.set_yticks(ticks[1:-1])
 
-# load andrew's lcs
-ax2 = fig.add_subplot(212)
-x, y, _ = np.genfromtxt("../data/c1/ep%s.csv" % eid, delimiter=",").T
-x *= 24*3600
-model = LombScargle().fit(x, y, np.ones_like(y)*1e-5)
-ps = 1. / fs
-pgram = model.periodogram(ps)
-ax2.plot(fs[::3]*1e6, pgram[::3], "k")
-ax.set_xlabel("$\\nu\mathrm{~(}\mu\mathrm{Hz)}$")
-ax2.set_xlim(10, 280)
-ax2.set_ylim(0, .015)
-fig.subplots_adjust(hspace=0)
-fig.text(0.04, 0.5, "$\mathrm{Power}$", ha="center", va="center",
-         rotation="vertical")
-plt.savefig("../documents/rawvbg_%s.pdf" % eid)
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['right'].set_color('none')
+    ax.tick_params(labelcolor='w', top='off', bottom='off', left='off',
+                   right='off')
+
+    # load andrew's lcs
+    ax2 = fig.add_subplot(312)
+    x, y, _ = np.genfromtxt("../data/c1/ep%s.csv" % eid, delimiter=",").T
+    x *= 24*3600
+    model = LombScargle().fit(x, y, np.ones_like(y)*1e-5)
+    ps = 1. / fs
+    pgram = model.periodogram(ps)
+    ax2.plot(fs[::3]*1e6, pgram[::3], "k")
+    ax2.set_xlim(10, 280)
+    ax2.set_ylim(0, .015)
+    plt.ylabel("$\mathrm{Power}$")
+    ticks = ax2.get_yticks()
+    print ticks
+    ax2.set_yticks(ticks[1:-1])
+#     fig.text(0.04, 0.5, "$\mathrm{Power}$", ha="center", va="top",
+#              rotation="vertical")
+
+    # load sip
+    fs, s2n = np.genfromtxt("../astero/%sastero_pgram.txt"
+                            % str(int(eid))).T
+    ax3 = fig.add_subplot(313)
+    plt.plot(fs[::3], s2n[::3]*10e4, "k")
+    ax3.set_xlim(10, 280)
+    plt.ylabel("$\mathrm{S/N~(} \\times 10^{-4}\mathrm{)}$")
+    plt.xlabel("$\\nu\mathrm{~(}\mu\mathrm{Hz)}$")
+    fig.subplots_adjust(hspace=0, bottom=.1)
+    ticks = ax3.get_yticks()
+    print ticks
+    ax3.set_yticks(ticks[1:-1])
+    plt.savefig("../documents/rawvbg_%s.pdf" % eid)
+
+if __name__ == "__main__":
+    raw_and_vbg()
