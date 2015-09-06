cc017f6c4797f681d8c9cb6a3fe1e91682bac5f5
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-09-06 14:56:28 -0400

    sigma clipping

--- stello/stello.py
+++ stello/stello.py
@@ -9,6 +9,44 @@ reb = params()
 from WD import EPICSIP
 from SIP import SIP, eval_freq
 
+def sigma_clipping(x, y, low, high):
+
+    plt.clf()
+    plt.plot(x, y, "r.")
+
+    # running median with 100 points
+    N = 100
+    nsections = len(x)/N + 1
+    inds = range(0, nsections*100, 100)
+
+    # middle bit
+    meds = []
+    for i, ind0 in enumerate(inds):
+        ind1 = ind0 + N
+        sx, sy = x[ind0:ind1], y[ind0:ind1]
+        med, std = np.median(sy[np.isfinite(sy)]), np.std(sy[np.isfinite(sy)])
+        m = (med - std*low < sy) * (sy < med + std*high)
+        x[ind0:ind1][~m] = np.nan
+        y[ind0:ind1][~m] = np.nan
+        meds.append(med)
+
+    # end bit
+    sx, sy = x[inds[-1]:], y[inds[-1]:]
+    med, std = np.median(sy[np.isfinite(sy)]), np.std(sy[np.isfinite(sy)])
+    m = (sy < med - std*low) * (med + std*high < sy)
+    x[inds[-1]:][~m] = np.nan
+    y[inds[-1]:][~m] = np.nan
+
+    # get rid of initial dip
+    clip = 170
+    y[:clip], x[:clip] = np.ones(clip)*np.nan, np.ones(clip)*np.nan
+
+    m = np.isfinite(y) * np.isfinite(x)
+    plt.plot(x[m], y[m], "k.")
+    plt.show()
+
+    return x, y
+
 def load_data(id):
         print "loading data"
         fname = "ktwo%s-c01_lpd-lc.fits" % id
@@ -17,37 +55,18 @@ def load_data(id):
         y = data["flux"][:, np.argmin(aps["cdpp6"])]
         x = data["time"]
         q = data["quality"]
+
+        # sigma clip
+        x, y = sigma_clipping(x, y, 4, 4)
         l = np.isfinite(y) * np.isfinite(x) * (q==0)
         y, x = y[l], x[l]
+
         MAD = np.median(y - np.median(y))
         x *= 24*3600
 
         print "load basis"
         with h5py.File("../data/c1.h5", "r") as f:
             basis = f["basis"][:150, l]
-
-        # inject sinusoid at 1000 ppm
-        med = np.median(y)
-        amp = med / 1e3
-        print med
-        print amp
-        y += amp*np.sin(x*2*np.pi*200*1e-6)
-
-        y = y / med - 1
-
-        print "computing SIP..."
-        fs = np.arange(10, 300, 4e-2) * 1e-6
-        amp2s, s2n, w = K2pgram(x, y, basis, fs)
-        print max(amp2s**.5)
-
-        plt.clf()
-        plt.plot(fs[::5], amp2s[::5]**.5*1e6, "k")
-        plt.xlabel("$\\nu\mathrm{~(}\mu\mathrm{Hz)}$")
-        plt.ylabel("$\mathrm{Relative~(S/N)}^2$")
-        plt.title("$\mathrm{EPIC~%s}$" % str(int(id)))
-        plt.subplots_adjust(bottom=.15, left=.2)
-        plt.savefig("test")
-        assert 0
 
         return x, y, basis
 
@@ -67,10 +86,7 @@ def list_SIP(epids):
         fs = np.arange(10, 300, 4e-2) * 1e-6
         amps2, s2n, w = K2pgram(x, y, basis, fs)
 
-        if MAD == 0: MAD = 1
-        print MAD
-#         signal = s2n/MAD**2  # normalising
-        data = np.vstack((fs, amps2**.5*med)).T
+        data = np.vstack((fs, amps2**.5*1e6)).T
 
         print "saving SIP"
         np.savetxt("%s.txt" % id, data)
@@ -78,7 +94,6 @@ def list_SIP(epids):
 if __name__ == "__main__":
     epids = np.genfromtxt("ktwo_c1_APO-RGs_llc.dat.epic.list", dtype=str).T
     list_SIP(epids)
-    assert 0
 
     for id in epids:
         fs, amp2s = np.genfromtxt("%s.txt" % id).T
