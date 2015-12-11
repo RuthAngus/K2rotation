278bb7a80836accd0c1f3879890771177c4779bf
Ruth Angus <ruth.angus@astro.ox.ax.uk>
2015-12-11 08:14:37 +0000

    plotting and smaller grid

--- code/inject.py
+++ code/inject.py
@@ -27,6 +27,10 @@ def prewhiten(x, y, f):
 
 # inject a sine wave
 def inject(x, y, f, a):
+    """
+    takes x, y, frequency and amplitude
+    returns y + wave
+    """
     wave = a * np.sin(f*2*np.pi*x)
     return y + wave
 
@@ -38,24 +42,24 @@ if __name__ == "__main__":
     x, y, basis = load_K2_data(fnames[0])
 
     # compute a SIP
-    fs = np.arange(10, 30, 1e-1) * 1e-6
+    fs = np.arange(10, 300, 1e-1) * 1e-6
     s2n, amps2, w = SIP(x, y, basis, fs)
 
     # inject a bunch of sinewaves
     a_s = np.ones_like(fs) * 1e-3
-    recovered = np.zeros_like(fs)
-    for i, f in enumerate(fs):
-        print(i, "of", len(fs))
+    recovered = np.zeros_like(fs[10:20])
+    for i, f in enumerate(fs[10:20]):
+        print(i, "of", len(fs[10:20]))
         y += inject(x, y, f, 1e-3)
         s2n, amps2, w = SIP(x, y, basis, fs)
         peak_f, _ = peak_detect(fs, amps2)
-        recovered[i] = peak_f
         plt.clf()
         plt.plot(fs, amps2)
-        plt.axvline(peak_f)
+        plt.axvline(peak_f, color="r")
         plt.show()
+        recovered[i] = peak_f
 
-    diff = (fs - recovered) / fs
+    diff = (fs[10:20] - recovered)
     plt.clf()
     plt.hist(diff)
     plt.savefig("test")
