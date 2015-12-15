0093d1ccae38d91f8d6c0375fb65a099b5c29004
RuthAngus <ruth.angus@astro.ox.ac.uk>
2015-12-15 16:55:44 +0000

    decided not to use S to N because it is hard to compute from light curve

--- code/inject.py
+++ code/inject.py
@@ -1,10 +1,12 @@
 import numpy as np
 from recover import inj
 import sys
+from K2misc import load_K2_data
 
 N = int(sys.argv[1])  # the number of injections
-ifs = np.random.uniform(10e-6, 300e-6, N) * 1e-6  # injected freqs
-a_s = 10**np.random.uniform(-4, -3, N)  # injection amps
-fname = "data/ktwo201121245-c01-lpd-lc.fits"
+ifs = np.random.uniform(10e-6, 300e-6, N)  # injected freqs
+a_s = 10**np.random.uniform(-5, -3, N)  # injection amps
+fname = "data/ktwo201121245-c01_lpd-lc.fits"
+x, y, basis = load_K2_data(fname)
 
 inj(fname, ifs, a_s)
