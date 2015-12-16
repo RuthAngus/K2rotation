5879e441c6547c48f9887ec61b7a994c66f57c9c
RuthAngus <ruth.angus@astro.ox.ac.uk>
2015-12-16 15:43:27 +0000

    only going up to 280Hz - nyquist is 283

--- code/inject.py
+++ code/inject.py
@@ -4,7 +4,7 @@ import sys
 from K2misc import load_K2_data
 
 N = int(sys.argv[1])  # the number of injections
-ifs = np.random.uniform(10e-6, 300e-6, N)  # injected freqs
+ifs = np.random.uniform(10e-6, 280e-6, N)  # injected freqs
 a_s = 10**np.random.uniform(-4, -3, N)  # injection amps
 fname = "data/ktwo201121245-c01_lpd-lc.fits"
 x, y, basis = load_K2_data(fname)
