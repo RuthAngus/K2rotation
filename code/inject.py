2c3805d151e7c2977df3f55f482826e75afcc271
RuthAngus <ruth.angus@astro.ox.ac.uk>
2015-12-16 18:13:57 +0000

    ppm

--- code/inject.py
+++ code/inject.py
@@ -9,8 +9,12 @@ rotation = False
 ifs = np.random.uniform(10e-6, 270e-6, N)  # injected freqs (astero)
 if rotation:
     ifs = 1./(np.random.uniform(1, 30, N) * 24 * 3600)  # injected freqs (rot)
-a_s = 10**np.random.uniform(-7, -2, N)  # injection amps
+a_s = 10**np.random.uniform(-7, -3, N)  # injection amps
+
 fname = "data/ktwo201121245-c01_lpd-lc.fits"
-x, y, basis = load_K2_data(fname)
+x, y, basis, med = load_K2_data(fname)
+# the light curve has been median normalised, so what used to be units of flux
+# is now units of flux / median
+a_s = 10**np.random.uniform(1, 3, 1000) * (med/1e6) # convert to ppm
 
 inj(fname, ifs, a_s, rotation=rotation)
