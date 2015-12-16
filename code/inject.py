2b47d04352f23a16fc8ebcb5042149a752022113
RuthAngus <ruth.angus@astro.ox.ac.uk>
2015-12-16 17:47:56 +0000

    changing ranges

--- code/inject.py
+++ code/inject.py
@@ -4,9 +4,13 @@ import sys
 from K2misc import load_K2_data
 
 N = int(sys.argv[1])  # the number of injections
-ifs = np.random.uniform(10e-6, 280e-6, N)  # injected freqs
-a_s = 10**np.random.uniform(-4, -3, N)  # injection amps
+rotation = False
+
+ifs = np.random.uniform(10e-6, 270e-6, N)  # injected freqs (astero)
+if rotation:
+    ifs = 1./(np.random.uniform(1, 30, N) * 24 * 3600)  # injected freqs (rot)
+a_s = 10**np.random.uniform(-7, -2, N)  # injection amps
 fname = "data/ktwo201121245-c01_lpd-lc.fits"
 x, y, basis = load_K2_data(fname)
 
-inj(fname, ifs, a_s)
+inj(fname, ifs, a_s, rotation=rotation)
